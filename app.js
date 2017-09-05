ecd7b0cf0be4ab46e43580effd06a3ae1c852bfe
Luiz Felippe Lourenço Gonçalves <luiz.felippe@caseimagine.com>
2017-09-05 14:50:45 -0300

    Server config updated

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log(' ------------------------------------- \n' +
-    '--> Server listening on port: ' + port);
+                '--> Server listening on port: ' + port);
 });
